2a478b9c90ff0e180913410c0729399271f9d7fb
Aleksey <1862647@gmail.com>
2023-06-12 00:48:43 +0700

    fix: linter, prettier

--- src/services/store.js
+++ src/services/store.js
@@ -2,8 +2,12 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
-const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-  : compose;
+const composeEnhancers =
+	typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+		: compose;
 
-export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+export const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk)),
+);
