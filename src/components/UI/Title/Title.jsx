26222939ec45faa6af82c41734f72cefd74edcd4
Natalya Maximova <natka-31@mail.ru>
2023-06-13 14:08:44 +0500

    feat: add Title  and fix button components

---
+++ src/components/UI/Title/Title.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import styles from './Title.module.css';
+
+function Title({ titleText }) {
+  return (
+    <h2 className={styles.title}>
+      {titleText}
+    </h2>
+  );
+}
+
+Title.propTypes = {
+  titleText: PropTypes.string.isRequired,
+};
+
+export default Title;
