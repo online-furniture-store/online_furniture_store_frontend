df5d6a4afd57730255d808b7d1c46ac2d7d614b5
Kseniia Shirokova <ksirokova85@gmail.com>
2023-06-14 15:29:46 +0300

    hot-fixes 3.0

--- src/components/UI/grocery/grocery.jsx
+++ src/components/UI/grocery/grocery.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import styles from './grocery.module.css';
-import grosery from '../../../assets/img/grocery.svg';
+import groсery from '../../../assets/img/grocery.svg';
 
 function Grocery({ amount }) {
 	return (
 		<div className={styles.grocery}>
-			<img src={grosery} alt="grosery" />
+			<img src={groсery} alt="groсery" />
 			{amount !== 0
 			&& <div className={styles.circle}>{amount}</div>}
 		</div>
