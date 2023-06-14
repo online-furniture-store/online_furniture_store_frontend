a745829591722676ef7644a38de49698a32145cb
Kseniia Shirokova <ksirokova85@gmail.com>
2023-06-14 20:16:27 +0300

    fix: rewrite onchange function

--- src/components/Sections/Main/Footer/Footer.jsx
+++ src/components/Sections/Main/Footer/Footer.jsx
@@ -7,124 +7,124 @@ import vk from '../../../../assets/img/vk.svg';
 import EnteringEmail from '../../../UI/EnteringEmail/EnteringEmail';
 
 function Footer() {
-	const [email, setEmail] = useState('');
-	const onChange = (e) => {
-		// eslint-disable-next-line no-unused-expressions
-		e.nativeEvent.inputType !== 'deleteContentBackward'
-			? setEmail(email + e.nativeEvent.data)
-			: setEmail(email.slice(0, -1));
-		// console.log(email);
-	};
-	return (
-		<section className={styles.footer}>
-			<div className={styles.column}>
-				<h3 className={styles.caption}>Свяжитесь с нами</h3>
-				<ul>
-					<li className={styles.link}>
-						<a
-							href="tel:+7 (495) 555–55–55"
-							className={styles.link}
-							target="_blank"
-							rel="noreferrer"
-						>
-							+7 (495) 555–55–55
-						</a>
-					</li>
-					<li className={styles.link}>
-						<a
-							href="mailto:furniture@ofs.ru"
-							className={styles.link}
-							target="_blank"
-							rel="noreferrer"
-						>
-							furniture@ofs.ru
-						</a>
-					</li>
-					<li className={styles.link}>
-						<a
-							href="mailto:furniture@ofs.ru"
-							className={styles.link}
-							target="_blank"
-							rel="noreferrer"
-						>
-							Напишите нам
-						</a>
-					</li>
-					<li className={styles.link}>
-						<a
-							href="https://www.google.ru/maps/place/Светофор/@59.9542371,30.3436811,13z/data=!4m6!3m5!1s0x469633b689faf77d:0x47ec1ceb04e69d09!8m2!3d59.9656715!4d30.3679532!16s%2Fg%2F11lf_n909j?entry=ttu"
-							className={styles.link}
-							target="_blank"
-							rel="noreferrer"
-						>
-							Адрес
-						</a>
-					</li>
-					<li className={styles.link}>
-						<a
-							href="https://web.telegram.org/k/"
-							className={styles.image}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img src={telegram} alt="telegram" />
-						</a>
-						<a
-							href="http://whatsapp.com"
-							className={styles.image}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img src={whatsUp} alt="whats-up" className={styles.image} />
-						</a>
-						<a
-							href="https://m.vk.com/feed"
-							className={styles.image}
-							target="_blank"
-							rel="noreferrer"
-						>
-							<img src={vk} alt="vk" className={styles.image} />
-						</a>
-					</li>
-				</ul>
-			</div>
-			<div className={styles.column}>
-				<h3 className={styles.caption}>Информация для покупателей</h3>
-				<ul>
-					<li>
-						<NavLink to="/" className={styles.link}>
-							Доставка и самовывоз
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/" className={styles.link}>
-							Как заказать
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/" className={styles.link}>
-							Как оплатить
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/" className={styles.link}>
-							Гарантия и возврат
-						</NavLink>
-					</li>
-					<li>
-						<NavLink to="/" className={styles.link}>
-							Политика конфиденциальности
-						</NavLink>
-					</li>
-				</ul>
-			</div>
-			<div className={styles.column}>
-				<h3 className={styles.caption}>Подписаться на рассылку</h3>
-				<EnteringEmail placeholder="Email" onChange={onChange} value={email} />
-				<p>2023, © Online Furniture Store</p>
-			</div>
-		</section>
-	);
+    const [email, setEmail] = useState(' ');
+    const onChange = (e) => {
+        if (e.nativeEvent.inputType !== 'deleteContentBackward') {
+            setEmail(email + e.nativeEvent.data);
+        } else {
+            setEmail(email.slice(0, -1));
+        }
+};
+return (
+    <section className={styles.footer}>
+        <div className={styles.column}>
+            <h3 className={styles.caption}>Свяжитесь с нами</h3>
+            <ul>
+                <li className={styles.link}>
+                    <a
+                        href="tel:+7 (495) 555–55–55"
+                        className={styles.link}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        +7 (495) 555–55–55
+                    </a>
+                </li>
+                <li className={styles.link}>
+                    <a
+                        href="mailto:furniture@ofs.ru"
+                        className={styles.link}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        furniture@ofs.ru
+                    </a>
+                </li>
+                <li className={styles.link}>
+                    <a
+                        href="mailto:furniture@ofs.ru"
+                        className={styles.link}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Напишите нам
+                    </a>
+                </li>
+                <li className={styles.link}>
+                    <a
+                        href="https://www.google.ru/maps/place/Светофор/@59.9542371,30.3436811,13z/data=!4m6!3m5!1s0x469633b689faf77d:0x47ec1ceb04e69d09!8m2!3d59.9656715!4d30.3679532!16s%2Fg%2F11lf_n909j?entry=ttu"
+                        className={styles.link}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Адрес
+                    </a>
+                </li>
+                <li className={styles.link}>
+                    <a
+                        href="https://web.telegram.org/k/"
+                        className={styles.image}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={telegram} alt="telegram" />
+                    </a>
+                    <a
+                        href="http://whatsapp.com"
+                        className={styles.image}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={whatsUp} alt="whats-up" className={styles.image} />
+                    </a>
+                    <a
+                        href="https://m.vk.com/feed"
+                        className={styles.image}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={vk} alt="vk" className={styles.image} />
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div className={styles.column}>
+            <h3 className={styles.caption}>Информация для покупателей</h3>
+            <ul>
+                <li>
+                    <NavLink to="/" className={styles.link}>
+                        Доставка и самовывоз
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" className={styles.link}>
+                        Как заказать
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" className={styles.link}>
+                        Как оплатить
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" className={styles.link}>
+                        Гарантия и возврат
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" className={styles.link}>
+                        Политика конфиденциальности
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
+        <div className={styles.column}>
+            <h3 className={styles.caption}>Подписаться на рассылку</h3>
+            <EnteringEmail placeholder="Email" onChange={onChange} value={email} />
+            <p>2023, © Online Furniture Store</p>
+        </div>
+    </section>
+);
 }
 
 export default Footer;
