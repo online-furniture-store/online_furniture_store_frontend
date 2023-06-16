17674ca5414f2ae89c18dfacb91000ccadc0cd72
Anna Kesova <IZABELE-TAN4@yandex.ru>
2023-06-16 17:22:21 +0300

    feat: bargainAndServices

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,16 @@
 import styles from './HomePage.module.css';
 import Intro from '../../components/Sections/Main/Intro/Intro';
 import Categories from '../../components/Sections/Main/Categories/Categories';
+import Services from '../../components/Sections/Main/Services/Services';
+import Bargain from '../../components/Sections/Main/Bargain/Bargain';
 
 function HomePage() {
 	return (
 		<main className={styles.container}>
 			<Intro />
 			<Categories />
+			<Bargain />
+			<Services />
 		</main>
 	);
 }
