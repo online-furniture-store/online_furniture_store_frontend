a9fd8b3201702680fab29bbf8db3218054b01245
Aleksey <1862647@gmail.com>
2023-06-25 16:19:36 +0700

    feat: add popular products

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,7 +1,13 @@
 import CartSection from '../../components/Sections/Cart/CartSection/CartSection';
+import PopularProducts from '../../components/Sections/Main/PopularProducts/PopularProducts';
 
 function CartPage() {
-	return <CartSection />;
+	return (
+		<>
+			<CartSection />
+			<PopularProducts />
+		</>
+	);
 }
 
 export default CartPage;
