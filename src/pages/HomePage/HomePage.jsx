22c93dae72dd75e2a066731e12a44b08f125bc85
Kseniia Shirokova <ksirokova85@gmail.com>
2023-07-04 14:58:27 +0300

    feat: try to add validation

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,7 +10,6 @@ import PopularProducts from '../../components/Sections/PopularProducts/PopularPr
 import Categories from '../../components/Sections/Main/Categories/Categories';
 import Services from '../../components/Sections/Main/Services/Services';
 import ProductsWithScroll from '../../components/Sections/Main/ProductsWithScroll/ProductsWithScroll';
-import Courier from '../../components/Sections/Courier/Courier';
 
 function HomePage() {
 	const dispatch = useDispatch();
@@ -28,7 +27,6 @@ function HomePage() {
 			<PopularProducts />
 			<Services />
 			<ProductsWithScroll fastDelivery />
-			<Courier />
 		</>
 	);
 }
