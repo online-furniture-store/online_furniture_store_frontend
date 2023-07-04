07bd0f718619ba54dfbca5584f5fb0793f089229
Kseniia Shirokova <ksirokova85@gmail.com>
2023-07-04 15:16:23 +0300

    hot-fixes 2.0

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,6 @@ import PopularProducts from '../../components/Sections/PopularProducts/PopularPr
 import Categories from '../../components/Sections/Main/Categories/Categories';
 import Services from '../../components/Sections/Main/Services/Services';
 import ProductsWithScroll from '../../components/Sections/Main/ProductsWithScroll/ProductsWithScroll';
-import Courier from '../../components/Sections/Courier/Courier';
 
 function HomePage() {
 	return (
@@ -14,7 +13,6 @@ function HomePage() {
 			<PopularProducts />
 			<Services />
 			<ProductsWithScroll fastDelivery />
-			<Courier />
 		</>
 	);
 }
