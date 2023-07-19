a2ba615daa08d704f53b8105ef5a6f629f820d42
Denis Vakulenko <101655241+woofmgn@users.noreply.github.com>
2023-07-19 14:38:13 +0500

    refactor: added in linter config rules react/jsx-wrap-multilines

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,27 +2,27 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AboutPage, HomePage } from '../../pages';
-import OrderPage from '../../pages/OrderPage/OrderPage';
 import CartPage from '../../pages/CartPage/CartPage';
 import ConsentDataProcessing from '../../pages/ConsentDataProcessing/ConsentDataProcessing';
 import DataProcessingPolicy from '../../pages/DataProcessingPolicy/DataProcessingPolicy';
+import OrderPage from '../../pages/OrderPage/OrderPage';
 import OrderingForm from '../../pages/OrderingForm/OrderingForm';
 import PageInDevelopment from '../../pages/PageInDevelopment/PageInDevelopment';
 import TradingRules from '../../pages/TradingRules/TradingRules';
 import UserAccount from '../../pages/UserAccount/UserAccount';
-import { closeModal } from '../../store/modal/modal-slice';
 import { getCart } from '../../store/cart/cart-slice';
+import { closeModal } from '../../store/modal/modal-slice';
 import {
-  fetchCollections,
-  fetchPopularProducts,
-  fetchProducts,
+	fetchCollections,
+	fetchPopularProducts,
+	fetchProducts,
 } from '../../store/products/products-slice';
+import { modals } from '../../utils/modals';
 import { Breadcrumbs } from '../Breadcrumbs/Breadcrumbs';
+import Modal from '../Modals/Modal/Modal';
 import Footer from '../Sections/Footer/Footer';
 import Header from '../Sections/Header/Header';
 import { UserOrders } from '../UserOrders/UserOrders';
-import Modal from '../Modals/Modal/Modal';
-import { modals } from '../../utils/modals';
 import styles from './App.module.css';
 
 function App() {
@@ -45,7 +45,14 @@ function App() {
 					<Routes>
 						<Route path="/" element={<HomePage />} />
 						<Route path="/about" element={<AboutPage />} />
-            <Route path="/user" element={<UserAccount><div /></UserAccount>} />
+						<Route
+							path="/user"
+							element={
+								<UserAccount>
+									<div />
+								</UserAccount>
+							}
+						/>
 						<Route path="/chosen" element={<PageInDevelopment />} />
 						<Route path="/cart" element={<CartPage />} />
 						<Route path="/arm-chairs" element={<PageInDevelopment />} />
@@ -58,7 +65,14 @@ function App() {
 						<Route path="/rules-consent" element={<ConsentDataProcessing />} />
 						<Route path="/rules-data" element={<DataProcessingPolicy />} />
 						<Route path="/order-form" element={<OrderingForm />} />
-            <Route path="/user/my_orders" element={<UserAccount><UserOrders /></UserAccount>} />
+						<Route
+							path="/user/my_orders"
+							element={
+								<UserAccount>
+									<UserOrders />
+								</UserAccount>
+							}
+						/>
 						<Route path="/order" element={<OrderPage />} />
 					</Routes>
 				</main>
